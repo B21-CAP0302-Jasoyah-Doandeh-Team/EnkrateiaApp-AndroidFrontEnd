163bd7e7e77af8f2f5779c2abe6827610a24b482
anugrahzeputra <anugrahzeputra@gmail.com>
2021-05-29 23:16:18 +0700

    menambahkan splash screen

---
+++ app/src/main/java/ml/jasoyahdoandehgroup/androidteam/enkrateiaapp/Splash_Screen.kt
@@ -0,0 +1,11 @@
+package ml.jasoyahdoandehgroup.androidteam.enkrateiaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
