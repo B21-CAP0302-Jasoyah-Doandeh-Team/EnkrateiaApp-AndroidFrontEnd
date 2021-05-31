fb93b6b5938ad007bd3ab750113aa6f759d495d1
mnfaizp <zynthkas@gmail.com>
2021-05-31 23:49:22 +0700

    item message and chat fragmenr

---
+++ app/src/main/java/ml/jasoyahdoandehgroup/androidteam/enkrateiaapp/ui/chat/ChatFragment.kt
@@ -0,0 +1,12 @@
+package ml.jasoyahdoandehgroup.androidteam.enkrateiaapp.ui.chat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ml.jasoyahdoandehgroup.androidteam.enkrateiaapp.R
+
+class ChatFragment : Fragment() {
+
+}
