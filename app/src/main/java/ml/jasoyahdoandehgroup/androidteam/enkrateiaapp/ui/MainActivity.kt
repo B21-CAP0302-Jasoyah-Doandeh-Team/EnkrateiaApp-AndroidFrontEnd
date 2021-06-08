15989174285a941ab28db00d277cbfb4e41061a0
mnfaizp <zynthkas@gmail.com>
2021-06-08 07:08:35 +0700

    permission

--- app/src/main/java/ml/jasoyahdoandehgroup/androidteam/enkrateiaapp/ui/MainActivity.kt
+++ app/src/main/java/ml/jasoyahdoandehgroup/androidteam/enkrateiaapp/ui/MainActivity.kt
@@ -1,15 +1,25 @@
 package ml.jasoyahdoandehgroup.androidteam.enkrateiaapp.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import ml.jasoyahdoandehgroup.androidteam.enkrateiaapp.R
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recordButton: FloatingActionButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,5 +34,26 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        recordButton = findViewById(R.id.fab_record)
+        recordButton.isEnabled = false
+
+        initPermission()
+
+    }
+
+    private fun initPermission() {
+        if  (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.RECORD_AUDIO,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                111
+            )
+            recordButton.isEnabled = true
+        }
+
     }
 }
